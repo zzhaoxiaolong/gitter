9e4cd1cd0262f4e63ac5bf574c6702abf962a63d
zzhaoxiaolong <1525898472@qq.com>
2024-11-14 19:09:31 +0800

    冒泡排序

--- 1114.c
+++ 1114.c
@@ -6,7 +6,7 @@ int main()
     int a[10];
     for(int i=0;i<n;i++)
         scanf("%d",&a[i]);
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n-1;i++)
     {
         for(int j=0;j<n-i-1;j++)
         {
@@ -19,5 +19,6 @@ int main()
         }
     }
     for(int i=0;i<n;i++)
-        printf("%d",a[i]);
+        printf("%d ",a[i]);
+    return 0;
 }
