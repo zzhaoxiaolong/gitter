fe52da01ee71b281ca6a3d58cf37a300cf41c18d
zzhaoxiaolong <1525898472@qq.com>
2024-11-14 09:31:22 +0800

    冒泡排序

---
+++ 1114.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,t;
+    scanf("%d",&n);
+    int a[10];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+        printf("%d",a[i]);
+}
